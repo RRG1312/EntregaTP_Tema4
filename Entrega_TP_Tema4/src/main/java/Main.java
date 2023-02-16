978f216ef11beca84f3f2fbb6674e600d02bd618
Ruben <77282483+RRG1312@users.noreply.github.com>
2023-02-16 21:48:25 +0100

    Delete Entrega_TP_Tema4 directory

--- Entrega_TP_Tema4/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //Ejercicio1.notaMedia();
-        //Ejercicio2.notaMedia();
-        //Ejercicio3.notaMedia();
-        //Ejercicio4.adivinaNumero();
-        //Ejercicio5.adivinoNumero();
-    }
-}
